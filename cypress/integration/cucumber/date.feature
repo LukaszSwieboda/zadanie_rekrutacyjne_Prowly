7c25a595b29d6c4a91aa1c79aed17e7418b7c577
LukaszSwieboda <lukasz.swieboda.mail@gmail.com>
2021-07-24 14:42:57 +0200

    third test case by date between 2015-2017

--- cypress/integration/cucumber/date.feature
+++ cypress/integration/cucumber/date.feature
@@ -10,4 +10,11 @@ Feature: Search "prowly" on google.com by date
      When type "prowly after:2018" to search box and press enter
      Then I should see info after 2018 about "prowly" in the results
 
-    
+    Scenario: Search "prowly" on google.com by date between 2015-2017
+     Given I open home page
+     When type "prowly" to search box and press enter
+     When click Tools button
+     When click Whenever drop box
+     When choose date range
+     And fill in from 2015 to 2017
+     Then I should see info between 2015-2017 about "prowly" in the results
--- cypress/integration/cucumber/date/dateSteps.js
+++ cypress/integration/cucumber/date/dateSteps.js
@@ -20,3 +20,30 @@ When('type "prowly after:2018" to search box and press enter', () => {
 Then('I should see info after 2018 about "prowly" in the results', () => {
 	cy.contains('div', 'prowly').should('be.visible')
 })
+
+When('click Tools button', () => {
+	cy.get('#hdtb-tls').click()
+})
+
+When('type {string} to search box and press enter', () => {
+	cy.get('input[name="q"]').type('prowly{enter}')
+})
+
+When('click Whenever drop box', () => {
+	cy.get('div').contains('Kiedykolwiek').click()
+})
+
+When('choose date range', () => {
+	cy.get('div').contains('Zakres dat...').click()
+})
+
+And('fill in from 2015 to 2017', () => {
+    cy.get('#OouJcb').type('2015')
+    cy.get('#rzG2be').type('2017')
+    cy.contains('OK').should('have.class', 'Ru1Ao BwGU8e fE5Rge').click()
+})
+
+Then('I should see info between 2015-2017 about "prowly" in the results', () => {
+	cy.contains('div', 'prowly').should('be.visible')
+})
+
